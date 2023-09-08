88907e0b3585081ea145b3a752d102e4cde0f323
dhonataborges <dhonata.borges2017@gmail.com>
2023-09-08 13:33:12 -0300

    myportfolio

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img")
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 6){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 250}px)`
+
+}
+
+setInterval(carrossel, 1800)
